88a78126eabd49d464c0ca0a0650f3fb7aefabed
Moab Omena <1moabomena@gmail.com>
2019-11-23 13:10:16 -0300

    Saving a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -87,6 +87,7 @@ public class DepartmentListController implements Initializable {
 
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
 			
 			
